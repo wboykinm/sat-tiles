d13bcb48fcf3a26582af07bb1696bb2b26c835e4
Bill Morris <wboykinm@geosprocket.com>
2013-05-24 12:34:20 -0400

    initial push, nonfunctional

---
+++ js/raster.js
@@ -0,0 +1,7 @@
+var prefix = prefixMatch(["webkit", "ms", "Moz", "O"]);
+
+function prefixMatch(p) {
+  var i = -1, n = p.length, s = document.body.style;
+  while (++i < n) if (p[i] + "Transform" in s) return "-" + p[i].toLowerCase() + "-";
+  return "";
+}
